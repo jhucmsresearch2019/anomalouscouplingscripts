8f38d8fdcbb7665ec7f984d96a76f170d86a03e1
Jared Feingold <jfeingo2@jhu.edu>
2019-03-06 19:59:50 +0100

    Modified to produce ROC plots for up to three datasets

--- ROC.py
+++ ROC.py
@@ -11,11 +11,11 @@ def ROC():
     f2 = ROOT.TFile("~/work/JHUGen/JHUGenerator/A3_1.root")
     t2 = f2.Get("tree")
 
-    f3 = ROOT.TFile("./vbf/std_vs_a3_2.root")
+    f3 = ROOT.TFile("./vbf/std_vs_a3_4.root")
     t3 = f3.Get("dataset/TestTree")
     
-    f4 = ROOT.TFile("./vbf/std_vs_a3_3.root")
-    t4 = f3.Get("dataset/TestTree")
+    #f4 = ROOT.TFile("./vbf/std_vs_a3_3.root")
+    #t4 = f3.Get("dataset/TestTree")
 
 #---------------------#
 #Fill h1    
@@ -86,37 +86,38 @@ def ROC():
 #--------------------#
 #Fill h5 and h6
 
-    h5 = ROOT.TH1F("h5", "sig", 1000, -1, 1)
-    h5.SetDirectory(0)
-    h6 = ROOT.TH1F("h6", "background", 1000, -1, 1)
-    h6.SetDirectory(0)
+#    h5 = ROOT.TH1F("h5", "sig", 1000, -1, 1)
+#    h5.SetDirectory(0)
+#    h6 = ROOT.TH1F("h6", "background", 1000, -1, 1)
+#    h6.SetDirectory(0)
     
-    for entry in t4:
-        if t4.classID == 1:
-            h5.Fill(t4.BDTG)
-        elif t4.classID == 0:
-            h6.Fill(t4.BDTG)
-        else: assert False
+#    for entry in t4:
+#        if t4.classID == 1:
+#            h5.Fill(t4.BDTG)
+#        elif t4.classID == 0:
+#            h6.Fill(t4.BDTG)
+#        else: assert False
 
-    h5.Scale(1/h5.Integral())
-    h6.Scale(1/h6.Integral())
+#    h5.Scale(1/h5.Integral())
+#    h6.Scale(1/h6.Integral())
 
 #------------------#
 #Make ROC for h5 and h6    
 
-    e = array('d', [0]*1001)
-    f = array('d', [0]*1001)
+#    e = array('d', [0]*1001)
+#    f = array('d', [0]*1001)
 
-    for i in range(1000+1):
-        e[i] = h5.Integral(1,i)
-        f[i] = h6.Integral(1,i)
+#    for i in range(1000+1):
+#        e[i] = h5.Integral(1,i)
+#        f[i] = h6.Integral(1,i)
    
-    g3 = ROOT.TGraph(1001, f, e)
-    g3.SetTitle('ROC3')
-    g3.SetLineColor(ROOT.kGreen+3);
-    g3.Draw("L")
-            
-    save_as_file_name = "ROC3"
+#    g3 = ROOT.TGraph(1001, f, e)
+#    g3.SetTitle('ROC3')
+#    g3.SetLineColor(ROOT.kGreen+3);
+#    g3.Draw("L")
+    
+       
+    save_as_file_name = "ROC4"
 
     for ext in "png", "eps", "root", "pdf":
         c.SaveAs("~//www/ml/"+save_as_file_name+"." +ext)
